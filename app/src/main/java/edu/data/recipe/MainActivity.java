b33f097bca47c4bdc37e0f9de1cba0db415afd47
heavyrisem <pyo1748@gmail.com>
2020-09-29 18:29:23 +0900

    upload

--- app/src/main/java/edu/data/recipe/MainActivity.java
+++ app/src/main/java/edu/data/recipe/MainActivity.java
@@ -3,19 +3,32 @@ package edu.data.recipe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText searchBar = findViewById(R.id.searchBar);
+        searchBar.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    Log.d("OnEnter", (String)searchBar.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
-    EditText searchBar = findViewById(R.id.searchBar);
-
-    searchBar.OnEditorActionListener()
 
 }
